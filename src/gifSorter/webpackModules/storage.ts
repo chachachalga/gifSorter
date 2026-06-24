9231443088ef5b72c19e45352ab2c3427d632442
{chachachalga} <{dounty35@gmail.com}>
2026-06-25 01:44:35 +0200

    storage added

---
+++ src/gifSorter/webpackModules/storage.ts
@@ -0,0 +1,79 @@
+interface GifFolder {
+  name: string;
+  gifs: string[];
+}
+
+interface GifFolderStorage {
+  folders: Record<string, GifFolder>;
+  folderOrder: string[];
+  selectedFolder: string;
+}
+
+const DEFAULT_STORAGE = {
+  folders: {},
+  folderOrder: [],
+  selectedFolder: "all"
+};
+
+const STORAGE_KEY = "gifFolders";
+
+function getStorage(): GifFolderStorage {
+  const raw = moonlight.localStorage.getItem(STORAGE_KEY);
+  const myData = raw ? JSON.parse(raw) : null;
+
+  if (!myData) {
+    return DEFAULT_STORAGE;
+  } else {
+    return { ...DEFAULT_STORAGE, ...myData }; // merge in case i ever change smt
+  }
+}
+
+function saveStorage(data: GifFolderStorage): void {
+  moonlight.localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+function getFolders(): Record<string, GifFolder> {
+  return getStorage().folders;
+}
+
+function createFolder(folderName: string): void {
+  const storage = getStorage();
+  const newId = self.crypto.randomUUID();
+
+  storage.folders = {
+    ...storage.folders,
+    [newId]: { name: folderName, gifs: [] }
+  };
+
+  storage.folderOrder.push(newId);
+  storage.selectedFolder = newId;
+
+  saveStorage(storage);
+}
+
+function deleteFolder(id: string): void {
+  const storage = getStorage();
+  delete storage.folders[id];
+  storage.folderOrder = storage.folderOrder.filter((folderId) => folderId !== id);
+  storage.selectedFolder = "all"; // no need for if case, cause you can only delete the folder if you have it selected
+  saveStorage(storage);
+}
+
+// what if name empty? or too long? add limits?
+function renameFolder(id: string, newName: string): void {
+  const storage = getStorage();
+  storage.folders[id].name = newName;
+  saveStorage(storage);
+}
+
+function addGifToFolder(id: string, gifUrl: string): void {
+  const storage = getStorage();
+  storage.folders[id].gifs.push(gifUrl);
+  saveStorage(storage);
+}
+
+function removeGifFromFolder(id: string, gifUrl: string) {
+  const storage = getStorage();
+  storage.folders[id].gifs = storage.folders[id].gifs.filter((url) => url !== gifUrl);
+  saveStorage(storage);
+}
